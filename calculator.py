444a5099da065dcb0b2fe8607c9236f69874094c
njerivera <veronjeri85@gmail.com>
2019-04-14 22:17:37 +0300

    added calculator 1 examples

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add (a,b):
+	answer=a+b
+	return answer
+
+
+def wholenumberdivision (b,c):
+	answer =b//c
+	return answer
+
+def division (c,d):
+	answer=c/d
+	return answer
+
+def modulus (d,e):
+	answer=d%e
+	return answer
+def exponent (e,f):
+	answer=e**f
+	return answer
+def percentage (f,g):
+	answer=(f/g)*100
+	return answer
+def subtract(g,h):
+	answer=g-h
+	return answer
+def multiply(h,i):
+	answer = h*i
+	return answer
+
+
+	
+
+
+
+
